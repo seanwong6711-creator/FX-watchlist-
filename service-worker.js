db14565f825eae820af40d584f41b8faf72dfb6f
seanwong6711-creator <seanwong6711@hotmail.com>
2026-07-11 20:19:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,24 @@
-const CACHE_NAME = "fx-watchlist-v1";
+const CACHE = "fx-dashboard-v1";
 
-
-const FILES_TO_CACHE = [
-
-"./",
-
-"./index.html",
-
-"./style.css",
-
-"./app.js",
-
-"./currencies.json",
-
-"./manifest.json"
-
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./currencies.json",
+  "./manifest.json"
 ];
 
-
-
-// Install cache
-
-self.addEventListener(
-"install",
-event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-
-return cache.addAll(FILES_TO_CACHE);
-
-})
-
-);
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
-
-
-
-// Load from cache when offline
-
-self.addEventListener(
-"fetch",
-event => {
-
-event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-
-return response || fetch(event.request);
-
-})
-
-);
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
